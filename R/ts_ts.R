112472e5e9364052fb64f24596a75f0e50843b57
Tom Smith <ThomUK@users.noreply.github.com>
2023-11-03 23:35:20 +0000

    add behavour

---
+++ R/ts_ts.R
@@ -0,0 +1,10 @@
+#' Timestamp
+#'
+#' Returns a timestamp in a format convenient for filename versioning
+#'
+#' @return string in the format "year month day _ hour minute second"
+#' @export
+#'
+ts_ts <- function(){
+  format.Date(Sys.time(), format = "%Y%m%d_%H%M%S")
+}
