06963561f630681e6f7aeae064c92927685f0788
Tom Smith <ThomUK@users.noreply.github.com>
2023-11-05 00:33:16 +0000

    handle paginated api calls where there are more than 1000 records to retrieve

--- R/t_api_provider_trusts.R
+++ R/t_api_provider_trusts.R
@@ -30,18 +30,44 @@ t_api_provider_trusts <- function(role = "NHS TRUST", include_inactive = FALSE){
     active_string
   )
 
+  # use a preliminary call to check the total number of records to fetch
+  head <- httr::HEAD(url)
+  total_records <- head[["headers"]][["x-total-count"]]
+  message("There are ", total_records, " records to fetch")
+
+  # get the first page, shape the results into a dataframe, and check if there is another page
   response <- httr::GET(url)
-
-  # check that all the available records have been returned, if not throw a warning
-  returned_count <- response[["headers"]][["returned-records"]]
-  total_count <- response[["headers"]][["x-total-count"]]
-
-  if(total_count > returned_count) warning(total_count, " records are available, but only the first ", returned_count, " are being returned.")
-
-  message("Fetched ", returned_count, " Records.")
 
   data <- httr::content(response) |>
     tidyr::as_tibble() |>
     tidyr::unnest_wider("Organisations")
+
+  next_url <- response[["headers"]][["next-page"]]
+
+  # if there is another page, enter a while loop to fetch the rest of the data
+  while(!is.null(next_url)){
+    message("Fetching: ", next_url)
+
+    # get the new page's data
+    response <- httr::GET(next_url)
+
+    # shape the new data
+    new_data <- httr::content(response) |>
+      tidyr::as_tibble() |>
+      tidyr::unnest_wider("Organisations")
+
+    # append the new data
+    data <- rbind(data, new_data)
+
+    next_url <- response[["headers"]][["next-page"]]
+  }
+
+  # check that all the available records have been returned, if not throw a warning
+  # returned_count <- head[["headers"]][["returned-records"]]
+  total_count <- response[["headers"]][["x-total-count"]]
+
+  if(nrow(data) != total_records) stop("The number of rows in the dataframe is different to the number of records available.")
+
+  return(data)
 
 }
